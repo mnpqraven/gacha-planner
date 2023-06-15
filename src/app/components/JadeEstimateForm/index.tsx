76cb9bc3486ed89506c900d4bf69374dbfc3cfc8
Othi <mnpq.raven@gmail.com>
2023-06-15 17:32:26 +0700

    hide command center, change calendar key

--- src/app/components/JadeEstimateForm/index.tsx
+++ src/app/components/JadeEstimateForm/index.tsx
@@ -250,7 +250,7 @@ export default function JadeEstimateForm({
                       >
                         <span>Jump to ...</span>
                         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                          <span className="text-xs">⌘/Alt + K</span>
+                          <span className="text-xs">⌘/Alt + C</span>
                         </kbd>
                       </Button>
                       <CommandDialog open={open} onOpenChange={setOpen}>
