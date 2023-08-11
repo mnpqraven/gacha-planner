10a807a5f9d2f7aedf424942fe99a71a2ec07ce6
Othi <mnpq.raven@gmail.com>
2023-08-11 17:39:37 +0700

    edge config api

---
+++ src/app/api/patch_banners/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
+import { List } from "@/lib/generics";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function GET() {
+  const patch_banners = await get<PatchBanner[]>("patch_banners");
+  if (patch_banners)
+    return NextResponse.json<List<PatchBanner>>({ list: patch_banners });
+  return new NextResponse("getting edge config data failed", {
+    status: 400,
+    headers: { "content-type": "application/json" },
+  });
+}
+
+export interface PatchBanner {
+  phase: 1 | 2;
+  version: `${number}.${number}.${1 | 2}`;
+  chara: [number, number, number, number];
+  lc: [number, number, number, number];
+}
