409851b96bfa85053efab596ec1e3e3a8c91b66e
Othi <mnpq.raven@gmail.com>
2023-10-30 00:24:33 +0700

    chore

--- src/envSchema.mjs
+++ src/envSchema.mjs
@@ -3,15 +3,19 @@ import * as z from "zod";
 
 export const env = createEnv({
   server: {
-    EDGE_CONFIG: z.string().url(),
+    EDGE_CONFIG: z.string().url().min(1),
+    DB_URL: z.string().url().min(1),
   },
   client: {
-    NEXT_PUBLIC_BASE_URL: z.string().url(),
-    NEXT_PUBLIC_WORKER_API: z.string().url(),
+    NEXT_PUBLIC_BASE_URL: z.string().url().min(1),
+    NEXT_PUBLIC_WORKER_API: z.string().url().min(1),
+    NEXT_PUBLIC_DB_CENTER: z.string().url().min(1),
   },
   runtimeEnv: {
+    DB_URL: process.env["DB_URL"],
     EDGE_CONFIG: process.env["EDGE_CONFIG"],
     NEXT_PUBLIC_BASE_URL: process.env["NEXT_PUBLIC_BASE_URL"],
     NEXT_PUBLIC_WORKER_API: process.env["NEXT_PUBLIC_WORKER_API"],
+    NEXT_PUBLIC_DB_CENTER: process.env["NEXT_PUBLIC_DB_CENTER"],
   },
 });
